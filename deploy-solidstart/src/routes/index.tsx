c4d289bf9a3944fe79a7e8fd019c824251d99e52
Ali Mahdavi <alihmahdavi@gmail.com>
2026-03-26 10:34:53 +0100

    add SolidStart

---
+++ deploy-solidstart/src/routes/index.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        "font-family": "sans-serif",
+        display: "flex",
+        "justify-content": "center",
+        "align-items": "center",
+        height: "100vh",
+        margin: "0",
+        background: "#f4f4f4",
+      }}
+    >
+      <h1 style={{ "font-size": "3rem", color: "#2563eb" }}>Hello, Flux!</h1>
+    </main>
+  );
+}
