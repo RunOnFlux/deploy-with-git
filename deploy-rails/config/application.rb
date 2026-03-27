40b748aa235dddc46aa39baaa0018e8cd1b4206e
Ali Mahdavi <alihmahdavi@gmail.com>
2026-03-28 00:45:46 +0100

    fix

--- deploy-rails/config/application.rb
+++ deploy-rails/config/application.rb
@@ -1,14 +1,16 @@
-require 'rails'
-require 'action_controller/railtie'
-require 'action_view/railtie'
+require_relative "boot"
+
+require "rails"
+require "active_model/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+
+Bundler.require(*Rails.groups)
 
 module DeployRails
   class Application < Rails::Application
     config.load_defaults 7.1
     config.eager_load = true
-    config.consider_all_requests_local = false
-    config.log_level = :info
-    config.logger = Logger.new($stdout)
     config.secret_key_base = 'deploy-rails-hello-world-demo-secret-key-base-not-for-production-use'
   end
 end
